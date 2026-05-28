973f9d2337b7ba0fccd4bbdfa10e2c1957b35c79
Aaron <aaron@Aarons-MacBook-Air-8.local>
2026-05-28 14:17:30 -0400

    update installer scripts

--- packages/cloud-tools.sh
+++ packages/cloud-tools.sh
@@ -11,12 +11,31 @@ require_ubuntu
 install_snap_package terraform --classic
 install_snap_package aws-cli --classic
 
-if [[ -x /snap/bin/terraform && ! -e /usr/local/bin/terraform ]]; then
-  ln -s /snap/bin/terraform /usr/local/bin/terraform
+if [[ -x /snap/bin/terraform ]]; then
+  ln -sf /snap/bin/terraform /usr/local/bin/terraform
+else
+  cat > /usr/local/bin/terraform <<'EOF'
+#!/usr/bin/env sh
+exec snap run terraform "$@"
+EOF
+  chmod 0755 /usr/local/bin/terraform
 fi
 
-if [[ -x /snap/bin/aws && ! -e /usr/local/bin/aws ]]; then
-  ln -s /snap/bin/aws /usr/local/bin/aws
-elif [[ -x /snap/bin/aws-cli && ! -e /usr/local/bin/aws ]]; then
-  ln -s /snap/bin/aws-cli /usr/local/bin/aws
+if [[ -x /snap/bin/aws ]]; then
+  ln -sf /snap/bin/aws /usr/local/bin/aws
+elif snap run aws-cli.aws --version >/dev/null 2>&1; then
+  cat > /usr/local/bin/aws <<'EOF'
+#!/usr/bin/env sh
+exec snap run aws-cli.aws "$@"
+EOF
+  chmod 0755 /usr/local/bin/aws
+else
+  cat > /usr/local/bin/aws <<'EOF'
+#!/usr/bin/env sh
+exec snap run aws-cli "$@"
+EOF
+  chmod 0755 /usr/local/bin/aws
 fi
+
+/usr/local/bin/terraform version >/dev/null
+/usr/local/bin/aws --version >/dev/null
